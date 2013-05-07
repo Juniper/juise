e82c85f8b6742cc2b02c8219d7ec4de045cca82b
Phil Shafer <phil.shafer@gmail.com>
2013-05-07 17:48:02 +0000

    checkpoint prefs work

--- web/clira/clira.js
+++ web/clira/clira.js
@@ -23,339 +23,6 @@ jQuery(function ($) {
         $.clira = { };
 
     $.extend($.clira, {
-        prefs: { },
-        prefsInit: function prefsInit () {
-            $.clira.prefs_form = $.yform(prefs_options, "#prefs-dialog",
-                                         prefs_fields);
-            $.clira.prefs = $.clira.prefs_form.getData();
-            $("#prefs").button().click(function (event) {
-                $.dbgpr("prefsEdit:", event.type);
-                if ($.clira.prefs_form.shown()) {
-                    $.clira.prefs_form.close();
-
-                    /* Put the focus back where it belongs */
-                    $.clira.refocus();
-
-                } else {
-                    $.clira.prefs_form.open();
-                }
-            });
-
-            $("#prefs-main-form").dialog({
-                autoOpen: false,
-                height: 300,
-                width: 350,
-                modal: true,
-                buttons: {
-                    'Close': function() {
-                        $(this).dialog("close");
-                    }
-                },
-                close: function() {
-                },
-            });
-
-            $("#prefsbtn").click(function() {
-                $("#prefs-main-form").dialog("open");
-            });
-
-            /* Set Up Devices */
-            $("#prefs-devices-form").dialog({
-                autoOpen: false,
-                height: 600,
-                width: 800,
-                resizable: false,
-                modal: true,
-                buttons: {
-                    'Close': function() {
-                        $(this).dialog("close");
-                    }
-                },
-                close: function() {
-                },
-            });
-
-            $("#prefs-devices").click(function() {
-                $("#prefs-devices-form").dialog("open");
-
-                $("#prefs-devices-grid").jqGrid({
-                    url: '/clira/db.php?p=device_list',
-                    editurl: '/clira/db.php?p=device_edit',
-                    datatype: 'json',
-                    colNames: ['Name', 'Hostname', 'Port',
-                               'Username', 'Password', 'Save Password', ''],
-                    colModel: [
-                        {
-                            name: 'name',
-                            index: 'index',
-                            width: 90,
-                            editable: true,
-                            editrules: {
-                                required: true
-                            },
-                        },
-                        {
-                            name: 'hostname',
-                            index: 'hostname',
-                            width: 100,
-                            editable: true,
-                            editrules: {
-                                required: true
-                            },
-                        },
-                        {
-                            name: 'port',
-                            index: 'port',
-                            width: 50,
-                            editable: true
-                        },
-                        {
-                            name: 'username',
-                            index: 'username',
-                            width: 100,
-                            editable: true,
-                            editrules: {
-                                required: true
-                            },
-                        },
-                        {
-                            name: 'password',
-                            index: 'password',
-                            width: 40,
-                            editable: true,
-                            edittype: 'password',
-                            hidden: true,
-                            hidedlg: true,
-                            editrules: {
-                                edithidden: true,
-                            },
-                        },
-                        {
-                            name: 'save_password',
-                            index: 'save_password',
-                            width: 20,
-                            editable: true,
-                            edittype: 'checkbox',
-                            editoptions: {
-                                value: 'yes:no',
-                                defaultValue: 'no',
-                            },
-                            formatter: 'checkbox',
-                        },
-                        {
-                            name: 'action',
-                            index: 'action',
-                            width: 40,
-                            formatter: 'actions',
-                            formatoptions: {
-                                editformbutton: true,
-                                editOptions: {
-                                    closeAfterEdit: true,
-                                    afterShowForm: function ($form) {
-                                        var $dialog = $('#editmodprefs-devices-grid');
-                                        var grid = $('#prefs-devices-grid');
-                                        var coord = {};
-
-                                        coord.top = grid.offset().top + (grid.height() / 2);
-                                        coord.left = grid.offset().left + (grid.width() / 2) - ($dialog.width() / 2);
-
-                                        $dialog.offset(coord);
-                                    }
-                                },
-                                delOptions: {
-                                    afterShowForm: function ($form) {
-                                        var $dialog = $form.closest('div.ui-jqdialog');
-                                        var grid = $('#prefs-devices-grid');
-                                        var coord = {};
-
-                                        coord.top = grid.offset().top + (grid.height() / 2);
-                                        coord.left = grid.offset().left + (grid.width() / 2) - ($dialog.width() / 2);
-
-                                        $dialog.offset(coord);
-                                    }
-                                },
-                            },
-                        },
-                    ],
-                    rowNum: 10,
-                    sortname: 'name',
-                    autowidth: true,
-                    viewrecords: true,
-                    sortorder: 'asc',
-                    height: 400,
-                    pager: '#prefs-devices-pager',
-                    beforeSelectRow: function (rowid, e) {
-                        if (e.target.id == 'delete') {
-                            // Do our row delete
-                            alert('delete row ' + rowid);
-                        }
-                    },
-                }).navGrid('#prefs-devices-pager', {
-                    edit:false,
-                    add:true,
-                    del:false,
-                    search:false,
-                }, {
-                    //prmEdit
-                    closeAfterEdit: true
-                }, {
-                    //prmAdd,
-                    closeAfterAdd: true
-                });
-            });
-        
-            /* Set Up Groups */
-            $("#prefs-groups-form").dialog({
-                autoOpen: false,
-                height: 600,
-                width: 800,
-                resizable: false,
-                modal: true,
-                buttons: {
-                    'Close': function() {
-                        $(this).dialog("close");
-                    }
-                },
-                close: function() {
-                }
-            });
-
-            $("#prefs-groups").click(function() {
-                $("#prefs-groups-form").dialog("open");
-
-                $("#prefs-groups-grid").jqGrid({
-                    url: '/clira/db.php?p=group_list',
-                    editurl: '/clira/db.php?p=group_edit',
-                    datatype: 'json',
-                    colNames: ['Name', 'Members', ''],
-                    colModel: [
-                        {
-                            name: 'name',
-                            index: 'name',
-                            width: 90,
-                            editable: true,
-                            editrules: {
-                                required: true
-                            },
-                        },
-                        {
-                            name: 'members',
-                            index: 'devices',
-                            editable: true,
-                            edittype: 'select',
-                            editrules: {
-                                required: true,
-                            },
-                            editoptions: {
-                                multiple: true,
-                                dataUrl: '/clira/db.php?p=device',
-                                buildSelect: function (data) {
-                                    var j = $.parseJSON(data);
-                                    var s = '<select>';
-                                    if (j.devices && j.devices.length) {
-                                        $.each(j.devices, function (i, item) {
-                                            s += '<option value="' + item.id + '">' + item.name + '</option>';
-                                        });
-                                    }
-                                    return $(s)[0];
-                                },
-                            },
-                        },
-                        {
-                            name: 'action',
-                            index: 'action',
-                            width: 40,
-                            formatter: 'actions',
-                            formatoptions: {
-                                editformbutton: true,
-                                editOptions: {
-                                    closeAfterEdit: true,
-                                    afterShowForm: function ($form) {
-                                        var $dialog = $('#editmodprefs-groups-grid');
-                                        var grid = $('#prefs-groups-grid');
-                                        var coord = {};
-
-                                        coord.top = grid.offset().top + (grid.height() / 2);
-                                        coord.left = grid.offset().left + (grid.width() / 2) - ($dialog.width() / 2);
-
-                                        $dialog.offset(coord);
-                                    }
-                                },
-                                delOptions: {
-                                    afterShowForm: function ($form) {
-                                        var $dialog = $form.closest('div.ui-jqdialog');
-                                        var grid = $('#prefs-groups-grid');
-                                        var coord = {};
-
-                                        coord.top = grid.offset().top + (grid.height() / 2);
-                                        coord.left = grid.offset().left + (grid.width() / 2) - ($dialog.width() / 2);
-
-                                        $dialog.offset(coord);
-                                    }
-                                },
-                            },
-                        },
-                    ],
-                    rowNum: 10,
-                    sortname: 'name',
-                    autowidth: true,
-                    viewrecords: true,
-                    sortorder: 'asc',
-                    height: 400,
-                    pager: '#prefs-groups-pager',
-                    beforeSelectRow: function (rowid, e) {
-                        if (e.target.id == 'delete') {
-                            // Do our row delete
-                            alert('delete row ' + rowid);
-                        }
-                    },
-                }).navGrid('#prefs-groups-pager', {
-                    edit:false,
-                    add:true,
-                    del:false,
-                    search:false,
-                }, {
-                    //prmEdit
-                    closeAfterEdit: true
-                }, {
-                    //prmAdd,
-                    closeAfterAdd: true
-                });
-            });
-
-            $.extend(jQuery.jgrid.edit, { recreateForm: true });
-       
-            /* General Preferences */
-            $("#prefs-general").click(function() {
-                //$("#prefs-general-form").dialog("open");
-                // XXX: rkj    use new forms, decide what prefs we need?
-                if ($.clira.prefs_form.shown()) {
-                    $.clira.prefs_form.close();
-                    
-                    if (tgtHistory.value())
-                        cmdHistory.focus();
-                    else tgtHistory.focus();
-
-                } else {
-                    $.clira.prefs_form.open();
-                }
-            });
-        
-            $("#prefs-general-form").dialog({
-                autoOpen: false,
-                height: 600,
-                width: 800,
-                resizable: false,
-                modal: true,
-                buttons: {
-                    'Close': function() {
-                        $(this).dialog("close");
-                    }
-                },
-                close: function() {
-                }
-            });
-        },
         decorateIcons: function decorateIcons ($wrapper) {
             // Our container decorations (which need some of our functions)
             $(".icon-remove-section", $wrapper).text("Close").button({
@@ -457,95 +124,6 @@ jQuery(function ($) {
             $("#juise").button();
         },
     });
-
-    var prefs_fields = [
-        {
-            name: "output_close_after",
-            def: 5,
-            type: "number",
-            change: commandOutputTrimChanged,
-            title: "Number of open commands",
-        },
-        {
-            name: "output_remove_after",
-            def: 10,
-            type: "number",
-            change: commandOutputTrimChanged,
-            title: "Total number of commands",
-        },
-        {
-            name: "slide_speed",
-            def: 0, // "fast",
-            type: "number-plus",
-            title: "Speed of slide animations",
-        },
-        {
-            name: "max_commands_list",
-            def: 40,
-            type: "number",
-            title: "Number of commands kept in list",
-        },
-        {
-            name: "max_targets_inline",
-            def: 10,
-            type: "number",
-            title: "Number of targets kept on screen",
-        },
-        {
-            name: "max_targets_list",
-            def: 40,
-            type: "number",
-            title: "Number of targets kept in list",
-        },
-        {
-            name: "max_target_position",
-            def: 10,
-            type: "number",
-            title: "Target button rows",
-        },
-        {
-            name: "stay_on_page",
-            def: false,
-            type: "boolean",
-            label: "Stay",
-            change: prefsSetupConfirmExit,
-            title: "Give warning if leaving this page",
-        },
-        {
-            name: "theme",
-            def: "black-tie",
-            type: "string",
-            title: "UI Theme",
-            change: prefsSwitchTheme,
-        },
-        {
-            name: "live_action",
-            def: true,
-            type: "boolean",
-            label: "Live",
-            title: "Interact with real devices",
-        },
-        {
-            name: "mixer",
-            def: "ws://127.0.0.1:3000/mixer",
-            type: "string",
-            label: "Mixer Location",
-            title: "Address of the Mixer server",
-            change: prefsChangeMuxer,
-        },
-    ];
-
-    var prefs_options = {
-        preferences: {
-            apply: function () {
-                $.dbgpr("prefsApply");
-                $.clira.prefs_form.close();
-                $.clira.prefs = $.clira.prefs_form.getData();
-            }
-        },
-        title: "Preferences",
-        dialog : { },
-    }
 
     $.dbgpr("document is ready");
 
@@ -755,9 +333,7 @@ jQuery(function ($) {
             return false;
         });
 
-        var prefs_form = $.yform(prefs_options, "#prefs-dialog", prefs_fields);
-        prefs = prefs_form.getData();
-
+        $.clira.prefsInit();
     }
 
     function commandSubmit (event) {
@@ -1094,6 +670,20 @@ jQuery(function ($) {
                 },
             });
         },
+
+        prefsChangeMuxer: function prefsChangeMuxer (value, initial, prev) {
+            if (initial)
+                return;
+
+            if (muxer) {
+                muxer.close();
+                muxer = undefined;
+            }
+        },
+
+        commandOutputTrimChanged: function commandOutputTrimChanged () {
+            commandOutputTrim(0);
+        },
     });
 
     function promptForHostKey ($parent, prompt, onclick) {
@@ -1176,10 +766,6 @@ jQuery(function ($) {
                   .replace(/>/g, "&gt;");
     }
 
-    function commandOutputTrimChanged () {
-        commandOutputTrim(0);
-    }
-
     function commandOutputTrim (fresh_count) {
         var last = $output.get(0);
         if (!last)
@@ -1224,38 +810,6 @@ jQuery(function ($) {
         $(".icon-unhide-section", $wrapper).addClass("hidden");
         $(".icon-hide-section", $wrapper).removeClass("hidden");
         $(".can-hide", $wrapper).slideDown($.clira.prefs.slide_speed);
-    }
-
-    function prefsSetupConfirmExit () {
-        if ($.clira.prefs.stay_on_page) {
-            window.onbeforeunload = function (e) {
-                return "Are you sure?  You don't look sure.....";
-            }
-        } else {
-            window.onbeforeunload = null;
-        }
-    }
-
-    function prefsSwitchTheme (value, initial, prev) {
-        if (initial)
-            return;
-
-        $("link.ui-theme, link.ui-addon").each(function () {
-            var $this = $(this);
-            var attr = $this.attr("href");
-            attr = attr.replace(prev, value, "g");
-            $this.attr("href", attr);
-        });
-    }
-
-    function prefsChangeMuxer (value, initial, prev) {
-        if (initial)
-            return;
-
-        if (muxer) {
-            muxer.close();
-            muxer = undefined;
-        }
     }
 
     function hideCommandForm (yform) {
--- web/clira/clira.prefs.js
+++ web/clira/clira.prefs.js
@@ -13,6 +13,95 @@ jQuery(function ($) {
     if ($.clira == undefined)
         $.clira = { };
 
+    var prefs_fields = [
+        {
+            name: "output_close_after",
+            def: 5,
+            type: "number",
+            change: $.clira.commandOutputTrimChanged,
+            title: "Number of open commands",
+        },
+        {
+            name: "output_remove_after",
+            def: 10,
+            type: "number",
+            change: $.clira.commandOutputTrimChanged,
+            title: "Total number of commands",
+        },
+        {
+            name: "slide_speed",
+            def: 0, // "fast",
+            type: "number-plus",
+            title: "Speed of slide animations",
+        },
+        {
+            name: "max_commands_list",
+            def: 40,
+            type: "number",
+            title: "Number of commands kept in list",
+        },
+        {
+            name: "max_targets_inline",
+            def: 10,
+            type: "number",
+            title: "Number of targets kept on screen",
+        },
+        {
+            name: "max_targets_list",
+            def: 40,
+            type: "number",
+            title: "Number of targets kept in list",
+        },
+        {
+            name: "max_target_position",
+            def: 10,
+            type: "number",
+            title: "Target button rows",
+        },
+        {
+            name: "stay_on_page",
+            def: false,
+            type: "boolean",
+            label: "Stay",
+            change: prefsSetupConfirmExit,
+            title: "Give warning if leaving this page",
+        },
+        {
+            name: "theme",
+            def: "black-tie",
+            type: "string",
+            title: "UI Theme",
+            change: prefsSwitchTheme,
+        },
+        {
+            name: "live_action",
+            def: true,
+            type: "boolean",
+            label: "Live",
+            title: "Interact with real devices",
+        },
+        {
+            name: "mixer",
+            def: "ws://127.0.0.1:3000/mixer",
+            type: "string",
+            label: "Mixer Location",
+            title: "Address of the Mixer server",
+            change: $.clira.prefsChangeMuxer,
+        },
+    ];
+
+    var prefs_options = {
+        preferences: {
+            apply: function () {
+                $.dbgpr("prefsApply");
+                $.clira.prefs_form.close();
+                $.clira.prefs = $.clira.prefs_form.getData();
+            }
+        },
+        title: "Preferences",
+        dialog : { },
+    }
+
     $.extend($.clira, {
         prefs: { },
         prefsInit: function prefsInit () {
@@ -23,7 +112,7 @@ jQuery(function ($) {
         },
     });
 
-    function buildForm () {
+    function buildForms () {
         $("#prefs").button().click(function (event) {
             $.dbgpr("prefsEdit:", event.type);
             if ($.clira.prefs_form.shown()) {
@@ -350,6 +439,28 @@ jQuery(function ($) {
             },
             close: function() {
             }
+        });
+    }
+
+    function prefsSetupConfirmExit () {
+        if ($.clira.prefs.stay_on_page) {
+            window.onbeforeunload = function (e) {
+                return "Are you sure?  You don't look sure.....";
+            }
+        } else {
+            window.onbeforeunload = null;
+        }
+    }
+
+    function prefsSwitchTheme (value, initial, prev) {
+        if (initial)
+            return;
+
+        $("link.ui-theme, link.ui-addon").each(function () {
+            var $this = $(this);
+            var attr = $this.attr("href");
+            attr = attr.replace(prev, value, "g");
+            $this.attr("href", attr);
         });
     }
 });
