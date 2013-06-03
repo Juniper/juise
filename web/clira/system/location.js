5e1cbf7e333585c9e93bbb4e0bef1f747bd13fd8
Phil Shafer <phil@juniper.net>
2013-06-03 12:04:02 -0400

    Move to new CommandFile class

--- web/clira/system/location.js
+++ web/clira/system/location.js
@@ -9,28 +9,32 @@
  * LICENSE.
  */
 
-jQuery.clira.onload("location", function ($) {
-    $.clira.addCommand([
-        {
-            command: "show location",
-            arguments: [
-                {
-                    name: "location",
-                    type: "string",
-                    nokeyword: true,
-                    mandatory: true,
-                    multiple_words: true,
-                    help: "Name of a location to resolve",
-                },
-            ],
-            execute: geolocate,
-        },
-    ]);
+jQuery(function ($) {
+    jQuery.clira.commandFile({
+        name: "location",
+        handlebarsFile: "location.html",
+        commands: [
+            {
+                command: "show location",
+                arguments: [
+                    {
+                        name: "location",
+                        type: "string",
+                        nokeyword: true,
+                        mandatory: true,
+                        multiple_words: true,
+                        help: "Name of a location to resolve",
+                    },
+                ],
+                execute: geolocate,
+            },
+        ],
+    });
 
     function geolocate ($output, cmd, parse, poss) {
         var url = "http://maps.googleapis.com/maps/api/geocode/json";
         var me = parse.possibilities[0];
-
+        
         if (me.data.location) {
             var opts = {
                 sensor: "false",
@@ -43,37 +47,46 @@ jQuery.clira.onload("location", function ($) {
                     var lat = fixed4(res.geometry.location.lat);
                     var lng = fixed4(res.geometry.location.lng);
 
+//                    var template = this.handlebars("geotop");
+//                    var html = template(json);
+
                     var html = "<div>"
                         + "Address is: <span class='geo-address'>"
                         + res.formatted_address + "</span>"
                         + "</div>";
                     html += "<div class='location'>"
-                        + "system location latitude " + lat
+                        + "set system location latitude " + lat
                         + " longitude " + lng + "</div>";
 
                     // Only make the link if there's a map available
                     if (window.google && window.google.maps)
-                        + "<button class='link'></button>";
+                        html += "<button class='link'></button>";
 
                     html += "<div class='map-small hidden'></div>";
 
                     $output.html(html);
                     if (window.google && window.google.maps) {
                         var $map = $("div.map-small", $output);
-                        var $link = $("button.link", $output);
-                        $link.click(function (e) {
-                            $map.removeClass("hidden");
+                        $("button.link", $output).button({
+                            label: "Map it",
+                            icons: { primary: "ui-icon-flag" },
+                        })
+                        .click(function (e) {
+                            if ($map.hasClass("hidden")) {
+                                $map.removeClass("hidden");
 
-                            var map = new GMaps({
-                                div: $map.get(0),
-                                lat: lat,
-                                lng: lng,
-                            });
-                            map.addMarker({
-                                lat: lat,
-                                lng: lng,
-                                title: me.data.location,
-                            });
+                                var map = new GMaps({
+                                    div: $map.get(0),
+                                    lat: lat,
+                                    lng: lng,
+                                });
+                                map.addMarker({
+                                    lat: lat,
+                                    lng: lng,
+                                    title: me.data.location,
+                                });
+                            } else
+                                $map.addClass("hidden");
                         });
                     }
 
