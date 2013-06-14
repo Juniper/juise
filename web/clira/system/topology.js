eef513bc490a93e31e0b572336ef4ddb68660006
Phil Shafer <phil@juise.org>
2013-06-13 21:16:18 -0400

    writespace

--- web/clira/system/topology.js
+++ web/clira/system/topology.js
@@ -160,17 +160,17 @@ jQuery(function ($) {
 */
 
             force.on("tick", function() {
-                link.attr("x1", function(d) { return d.source.x; })
-                    .attr("y1", function(d) { return d.source.y; })
-                    .attr("x2", function(d) { return d.target.x; })
-                    .attr("y2", function(d) { return d.target.y; });
+                link.attr("x1", function (d) { return d.source.x; })
+                    .attr("y1", function (d) { return d.source.y; })
+                    .attr("x2", function (d) { return d.target.x; })
+                    .attr("y2", function (d) { return d.target.y; });
 
                 node.attr("transform", function (node) {
                     return "translate(" + node.x + "," + node.y + ")";
                 });
 /*
-                node.attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
+                node.attr("cx", function (d) { return d.x; })
+                    .attr("cy", function (d) { return d.y; });
 */
             });
         });
