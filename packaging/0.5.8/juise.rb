7411856e2a8f5fd7d33b0b358f454381bbe2dc3a
Phil Shafer <phil@juise.org>
2013-08-21 13:22:29 -0400

    new packaging data

---
+++ packaging/0.5.8/juise.rb
@@ -0,0 +1,31 @@
+#
+# Homebrew formula file for juise
+# https://github.com/mxcl/homebrew
+#
+
+require 'formula'
+
+class Juise < Formula
+  homepage 'https://github.com/Juniper/juise'
+  url 'https://github.com/Juniper/juise/releases/0.5.8/juise-0.5.8.tar.gz'
+  sha1 '4529b0d5cf08185d0f9e991aea8fc62468290d9c'
+
+  depends_on 'libtool' => :build
+  depends_on 'libslax'
+  depends_on 'libssh2'
+  depends_on 'pcre'
+
+  # Need newer versions of these libraries
+  if MacOS.version <= :lion
+    depends_on 'libxml2'
+    depends_on 'libxslt'
+    depends_on 'curl'
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-libssh2-prefix=#{HOMEBREW_PREFIX}"
+    system "make install"
+  end
+end
