32a8d61ab79656a1d94c62358dee6e9b936fd403
Phil Shafer <phil@juise.org>
2013-09-23 15:59:28 -0400

    add DBG_FLAG_DUMP

--- mixer/debug.h
+++ mixer/debug.h
@@ -14,6 +14,7 @@ extern unsigned opt_verbose;
 
 /* Flags for opt_debug: */
 #define DBG_FLAG_POLL	(1<<0)	/* poll()/poller related */
+#define DBG_FLAG_DUMP	(1<<1)	/* dump packet contents */
 
 #define MX_LOG(_fmt...) \
     do { if (opt_debug || opt_verbose) mx_log(_fmt); } while (0)
