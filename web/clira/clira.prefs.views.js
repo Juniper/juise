ef609d6b66c3f6726d9f7244136a39abe41fc6e0
ajhai <nitk.ajay@gmail.com>
2014-07-17 15:36:23 +0530

    Disable device groups

--- web/clira/clira.prefs.views.js
+++ web/clira/clira.prefs.views.js
@@ -76,7 +76,7 @@ Clira.PreferencesDialog = Ember.View.extend({
                 of: '#' + elementId 
             },
             resizable: false,
-            width: 320,
+            width: 220,
         });
     }
 });
