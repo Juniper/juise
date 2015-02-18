2197357313f48f466deb0c75ecd088f7f1ed2d35
Phil Shafer <phil@juise.org>
2015-02-17 23:52:57 -0500

    initial version

---
+++ delta/delta.h
@@ -0,0 +1,11 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012-2013, Juniper Networks, Inc.
+ * All rights reserved.
+ * This SOFTWARE is licensed under the LICENSE provided in the
+ * ../Copyright file. By downloading, installing, copying, or otherwise
+ * using the SOFTWARE, you agree to be bound by the terms of that
+ * LICENSE.
+ */
+
