87ab89da5b8ad3c1aff441c1c2e207623a12114c
Phil Shafer <phil@juise.org>
2015-02-17 23:51:05 -0500

    initial version

---
+++ libjuise/memory/error.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2008, Juniper Networks, Inc.
+ * All rights reserved.
+ *
+ */
+
+#include <stdlib.h>
+#include <ddl/memory.h>
+#include <ddl/dmalloc.h>
+
+malloc_error_func_t malloc_error_func;
+
+malloc_error_func_t
+malloc_error_func_set (malloc_error_func_t func)
+{
+    malloc_error_func_t old_func;
+
+    old_func = malloc_error_func;
+    malloc_error_func = func;
+
+    return old_func;
+}
+
+int
+dbm_mal_verify (dbm_memory_t *dbmp __unused, int fullcheck __unused)
+{
+    return 0;
+}
+
+void
+dbm_mal_debug (dbm_memory_t *dbmp __unused, int level __unused)
+{
+}
+
+void
+dbm_mal_trace (int value __unused)
+{
+}
+
+void
+dbm_mal_setstatsfile (FILE *fp __unused)
+{
+}
