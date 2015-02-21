f5f8befe1691063069be8e2d2940f567c5001fa6
Phil Shafer <phil@juise.org>
2015-02-21 18:15:32 -0500

    get it working

--- libjuise/memory/malloc.c
+++ libjuise/memory/malloc.c
@@ -5,17 +5,17 @@
  * Simple power of two allocator.
  *
  *
- * The allocated memory is split up in pages (PAGE_SIZE).
+ * The allocated memory is split up in pages (DBM_PAGE_SIZE).
  *
  *
- * Memory chunks that are > PAGE_SIZE / 4 occupy at least
+ * Memory chunks that are > DBM_PAGE_SIZE / 4 occupy at least
  * one page, or possibly more. No partial pages are allocated.
  *
  * The pool of free pages is maintained in dbmp->dm_free_pages,
  * indexed by the number of contiguous pages available.
  *
  *
- * Memory chunks that are <= PAGE_SIZE / 4 share a page
+ * Memory chunks that are <= DBM_PAGE_SIZE / 4 share a page
  * that is split up into equally sized chunks allocated to the next
  * power of two (function get_frag_idx).
  *
@@ -45,10 +45,28 @@
 #include <assert.h>
 #include <sys/types.h>
 #include <sys/errno.h>
+#include <string.h>
+
+#if 0
 #include <machine/cpufunc.h>
-#include <ddl/memory.h>
-#include <ddl/dmalloc.h>
-#include <ddl/object.h>
+#endif
+
+#include "juiseconfig.h"
+#include <libjuise/common/aux_types.h>
+#include <libjuise/memory/memory.h>
+#include <libjuise/memory/dmalloc.h>
+
+#if defined(__i386__) || defined(__amd64__)
+static inline u_int32_t
+bsrl32 (u_int32_t mask)
+{
+    u_int32_t   result;
+
+    __asm __volatile("bsrl %1,%0" : "=r" (result) : "rm" (mask));
+
+    return result;
+}
+#endif /* defined(__i386__) || defined(__amd64__) */
 
 /*
  * get_frag_idx
@@ -67,7 +85,7 @@ get_frag_idx (u_int32_t size)
 	mask = DBM_SLOT_MIN_SIZE - 1;
 
 #if defined(__i386__) || defined(__amd64__)
-    idx = bsrl(mask) + 1;
+    idx = bsrl32(mask) + 1;
 #else
     for (idx = 0; mask; idx++)
 	mask >>= 1;
@@ -89,6 +107,7 @@ get_frag_offset_p (dbm_memory_t *dbmp, u_int32_t size, u_int32_t *recorded_size_
     dbm_offset_t *frag_offset_p;
     u_int32_t recorded_size;
 
+#ifdef DO_NOT_SWEAT_DDL_OBJECT_SIZE
     if (size >= sizeof(ddl_object_t) &&
 	size <=  sizeof(ddl_object_t) + 8) {
 	if (size > sizeof(ddl_object_t) + 4) {
@@ -110,6 +129,7 @@ get_frag_offset_p (dbm_memory_t *dbmp, u_int32_t size, u_int32_t *recorded_size_
 	    return frag_offset_p;
 	}
     }
+#endif /* DO_NOT_SWEAT_DDL_OBJECT_SIZE */
 
     frag_idx = get_frag_idx(size);
     if (frag_idx < DBM_POW2_FRAG_COUNT) {
@@ -117,7 +137,7 @@ get_frag_offset_p (dbm_memory_t *dbmp, u_int32_t size, u_int32_t *recorded_size_
 	recorded_size = 4 << frag_idx;
     } else {
 	frag_offset_p = NULL;
-	recorded_size = (size + PAGE_SIZE - 1) & ~PAGE_MASK;
+	recorded_size = (size + DBM_PAGE_SIZE - 1) & ~DBM_PAGE_MASK;
     }
 
     if (recorded_size_p)
@@ -152,10 +172,10 @@ alloc_pages (dbm_memory_t *dbmp, u_int32_t page_count)
 	if (free_page == DBM_OFFSET_NULL)
 	    continue;
 
-	free_p = (dbm_free_page_t *)((caddr_t)dbmp + free_page);
+	free_p = (dbm_free_page_t *)(void *)((caddr_t)dbmp + free_page);
 
 	if (free_p->dfp_next != DBM_OFFSET_NULL) {
-	    next_free_p = (dbm_free_page_t *)((caddr_t)dbmp + free_p->dfp_next);
+	    next_free_p = (dbm_free_page_t *)(void *)((caddr_t)dbmp + free_p->dfp_next);
 	    next_free_p->dfp_prev = DBM_OFFSET_NULL;
 	}
 
@@ -164,15 +184,15 @@ alloc_pages (dbm_memory_t *dbmp, u_int32_t page_count)
 	if (page_count_probe > page_count) {
 	    /* register leftover pages */
 
-	    free_page_next = free_page + page_count * PAGE_SIZE;
+	    free_page_next = free_page + page_count * DBM_PAGE_SIZE;
 	    page_count_left = page_count_probe - page_count;
 
-	    free_p = (dbm_free_page_t *)((caddr_t)dbmp + free_page_next);
+	    free_p = (dbm_free_page_t *)(void *)((caddr_t)dbmp + free_page_next);
 	    free_p->dfp_prev = DBM_OFFSET_NULL;
 	    free_p->dfp_next = dbmp->dm_free_pages[page_count_left - 1];
 
 	    if (free_p->dfp_next != DBM_OFFSET_NULL) {
-		next_free_p = (dbm_free_page_t *)((caddr_t)dbmp + free_p->dfp_next);
+		next_free_p = (dbm_free_page_t *)(void *)((caddr_t)dbmp + free_p->dfp_next);
 		next_free_p->dfp_prev = free_page_next;
 	    }
 
@@ -182,15 +202,15 @@ alloc_pages (dbm_memory_t *dbmp, u_int32_t page_count)
 	return free_page;
     }
 
-    if ((dbmp->dm_top + PAGE_SIZE - 1) / PAGE_SIZE + page_count > DBM_MEMORY_PAGE_COUNT)
+    if ((dbmp->dm_top + DBM_PAGE_SIZE - 1) / DBM_PAGE_SIZE + page_count > DBM_MEMORY_PAGE_COUNT)
 	return DBM_OFFSET_NULL;
 
-    new_chunk = dbm_mmap(dbmp, page_count * PAGE_SIZE);
+    new_chunk = dbm_mmap(dbmp, page_count * DBM_PAGE_SIZE);
     if (new_chunk) {
 	free_page = (caddr_t)new_chunk - (caddr_t)dbmp;
 
-	assert(!(free_page & PAGE_MASK));
-	assert(free_page / PAGE_SIZE + page_count <= DBM_MEMORY_PAGE_COUNT);
+	assert(!(free_page & DBM_PAGE_MASK));
+	assert(free_page / DBM_PAGE_SIZE + page_count <= DBM_MEMORY_PAGE_COUNT);
 
 	return free_page;
     }
@@ -209,15 +229,15 @@ free_pages (dbm_memory_t *dbmp, dbm_offset_t start, u_int32_t page_count)
 {
     dbm_free_page_t *free_p, *next_free_p;
 
-    assert (!(start & PAGE_MASK) &&
+    assert (!(start & DBM_PAGE_MASK) &&
 	    page_count > 0 && page_count <= DBM_MAX_CONTIG_PAGES);
 
-    free_p = (dbm_free_page_t *)((caddr_t)dbmp + start);
+    free_p = (dbm_free_page_t *)(void *)((caddr_t)dbmp + start);
     free_p->dfp_prev = DBM_OFFSET_NULL;
     free_p->dfp_next = dbmp->dm_free_pages[page_count - 1];
 
     if (free_p->dfp_next != DBM_OFFSET_NULL) {
-	next_free_p = (dbm_free_page_t *)((caddr_t)dbmp + free_p->dfp_next);
+	next_free_p = (dbm_free_page_t *)(void *)((caddr_t)dbmp + free_p->dfp_next);
 	next_free_p->dfp_prev = start;
     }
 
@@ -238,15 +258,15 @@ dbm_malloc (dbm_memory_t *dbmp, size_t size)
 
     if (frag_offset_p) {
 	if (*frag_offset_p != DBM_OFFSET_NULL) {
-	    header_p = (dbm_page_header_t *)((caddr_t)dbmp + (*frag_offset_p & ~PAGE_MASK));
-	    slot_idx = ((*frag_offset_p & PAGE_MASK) - sizeof(dbm_page_header_t)) / recorded_size;
+	    header_p = (dbm_page_header_t *)(void *)((caddr_t)dbmp + (*frag_offset_p & ~DBM_PAGE_MASK));
+	    slot_idx = ((*frag_offset_p & DBM_PAGE_MASK) - sizeof(dbm_page_header_t)) / recorded_size;
 	    header_p->dph_usage[slot_idx / (sizeof(dbm_mask_t) * NBBY)] |=
 		1 << (slot_idx % (sizeof(dbm_mask_t) * NBBY));
 
-	    free_p = (dbm_free_slot_t *)((caddr_t)dbmp + *frag_offset_p);
+	    free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + *frag_offset_p);
 
 	    if (free_p->dfs_next != DBM_OFFSET_NULL) {
-		next_free_p = (dbm_free_slot_t *)((caddr_t)dbmp + free_p->dfs_next);
+		next_free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + free_p->dfs_next);
 		next_free_p->dfs_prev = DBM_OFFSET_NULL;
 	    }
 
@@ -257,7 +277,7 @@ dbm_malloc (dbm_memory_t *dbmp, size_t size)
 
 	page_count = 1;
     } else
-	page_count = (size + PAGE_SIZE - 1) / PAGE_SIZE;
+	page_count = (size + DBM_PAGE_SIZE - 1) / DBM_PAGE_SIZE;
 
     new_pages = alloc_pages(dbmp, page_count);
     if (new_pages == DBM_OFFSET_NULL) {
@@ -265,12 +285,12 @@ dbm_malloc (dbm_memory_t *dbmp, size_t size)
 	return NULL;
     }
 
-    size_info_p = dbmp->dm_size_info + new_pages / PAGE_SIZE;
+    size_info_p = dbmp->dm_size_info + new_pages / DBM_PAGE_SIZE;
     while (page_count > 0)
 	size_info_p[--page_count] = recorded_size;
 
     if (frag_offset_p) {
-	header_p = (dbm_page_header_t *)((caddr_t)dbmp + new_pages);
+	header_p = (dbm_page_header_t *)(void *)((caddr_t)dbmp + new_pages);
 	memset(header_p->dph_usage, 0, sizeof(header_p->dph_usage));
 	header_p->dph_usage[0] = 1; /* first one will be used */
 	    
@@ -278,10 +298,10 @@ dbm_malloc (dbm_memory_t *dbmp, size_t size)
 	*frag_offset_p = new_pages + offset;
 	prev_offset = DBM_OFFSET_NULL;
 	for (;;) {
-	    free_p = (dbm_free_slot_t *)((caddr_t)dbmp + new_pages + offset);
+	    free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + new_pages + offset);
 	    offset += recorded_size;
 	    free_p->dfs_prev = prev_offset;
-	    if (offset + recorded_size > PAGE_SIZE) {
+	    if (offset + recorded_size > DBM_PAGE_SIZE) {
 		free_p->dfs_next = DBM_OFFSET_NULL;
 		break;
 	    } else
@@ -306,14 +326,14 @@ dbm_free (dbm_memory_t *dbmp, void *ptr)
     dbm_free_slot_t *free_p, *prev_free_p, *next_free_p;
 
     slot_offset = (caddr_t)ptr - (caddr_t)dbmp;
-    page_offset = slot_offset & ~PAGE_MASK;
-    size_info_p = dbmp->dm_size_info + slot_offset / PAGE_SIZE;
+    page_offset = slot_offset & ~DBM_PAGE_MASK;
+    size_info_p = dbmp->dm_size_info + slot_offset / DBM_PAGE_SIZE;
 
     frag_offset_p = get_frag_offset_p(dbmp, *size_info_p, NULL);
 
     if (frag_offset_p) {
-	header_p = (dbm_page_header_t *)((caddr_t)dbmp + page_offset);
-	slot_idx = ((slot_offset & PAGE_MASK) - sizeof(dbm_page_header_t)) / *size_info_p;
+	header_p = (dbm_page_header_t *)(void *)((caddr_t)dbmp + page_offset);
+	slot_idx = ((slot_offset & DBM_PAGE_MASK) - sizeof(dbm_page_header_t)) / *size_info_p;
 	usage_p = header_p->dph_usage + slot_idx / (sizeof(dbm_mask_t) * NBBY);
 	mask = 1 << (slot_idx % (sizeof(dbm_mask_t) * NBBY));
 	if (!(*usage_p & mask))
@@ -323,12 +343,12 @@ dbm_free (dbm_memory_t *dbmp, void *ptr)
 
 	/* release this slot */
 
-	free_p = (dbm_free_slot_t *)((caddr_t)dbmp + slot_offset);
+	free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + slot_offset);
 	free_p->dfs_prev = DBM_OFFSET_NULL;
 	free_p->dfs_next = *frag_offset_p;
 
 	if (free_p->dfs_next != DBM_OFFSET_NULL) {
-	    next_free_p = (dbm_free_slot_t *)((caddr_t)dbmp + free_p->dfs_next);
+	    next_free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + free_p->dfs_next);
 	    next_free_p->dfs_prev = slot_offset;
 	}
 
@@ -353,25 +373,25 @@ dbm_free (dbm_memory_t *dbmp, void *ptr)
 	/* return page to pool */
 
 	for (offset = sizeof(dbm_page_header_t);
-	     offset + *size_info_p <= PAGE_SIZE;
+	     offset + *size_info_p <= DBM_PAGE_SIZE;
 	     offset += *size_info_p) {
-	    free_p = (dbm_free_slot_t *)((caddr_t)dbmp + page_offset + offset);
+	    free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + page_offset + offset);
 
 	    if (free_p->dfs_prev != DBM_OFFSET_NULL) {
-		prev_free_p = (dbm_free_slot_t *)((caddr_t)dbmp + free_p->dfs_prev);
+		prev_free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + free_p->dfs_prev);
 		prev_free_p->dfs_next = free_p->dfs_next;
 	    } else
 		*frag_offset_p = free_p->dfs_next;
 
 	    if (free_p->dfs_next != DBM_OFFSET_NULL) {
-		next_free_p = (dbm_free_slot_t *)((caddr_t)dbmp + free_p->dfs_next);
+		next_free_p = (dbm_free_slot_t *)(void *)((caddr_t)dbmp + free_p->dfs_next);
 		next_free_p->dfs_prev = free_p->dfs_prev;
 	    }
 	}
 
 	free_pages(dbmp, page_offset, 1);
     } else {
-	page_count = (*size_info_p + PAGE_SIZE - 1) / PAGE_SIZE;
+	page_count = (*size_info_p + DBM_PAGE_SIZE - 1) / DBM_PAGE_SIZE;
 	free_pages(dbmp, page_offset, page_count);
     }
 }
@@ -385,7 +405,7 @@ dbm_realloc (dbm_memory_t *dbmp, void *ptr, size_t size)
     if (!ptr)
 	return dbm_malloc(dbmp, size);
 
-    page = ((caddr_t)ptr - (caddr_t)dbmp) / PAGE_SIZE;
+    page = ((caddr_t)ptr - (caddr_t)dbmp) / DBM_PAGE_SIZE;
     size_info_p = dbmp->dm_size_info + page;
 
     if (size > *size_info_p) {
@@ -468,7 +488,7 @@ main(int argc, char **argv)
     dbmp = calloc(1, init_size);
 
     dbmp->dm_size = init_size;
-    dbmp->dm_top = (sizeof(*dbmp) + PAGE_SIZE - 1) & ~PAGE_MASK;
+    dbmp->dm_top = (sizeof(*dbmp) + DBM_PAGE_SIZE - 1) & ~DBM_PAGE_MASK;
 
     dbm_malloc_init(dbmp);
 
