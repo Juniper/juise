da770f4d718cc690ee45126b12192beb7d77328f
Phil Shafer <phil@juise.org>
2015-02-21 18:14:41 -0500

    Add test driver

--- delta/delta.c
+++ delta/delta.c
@@ -18,10 +18,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <err.h>
 
 #include "juiseconfig.h"
 #include <libjuise/common/aux_types.h>
 #include <libjuise/string/strextra.h>
+#include <libjuise/memory/memory.h>
+#include <libjuise/memory/dmalloc.h>
 
 #include "delta.h"
 
@@ -45,6 +48,25 @@ print_version (void)
     printf("libjuice version %s\n", LIBJUISE_VERSION);
 }
 
+#define ROUNDS 10000000
+
+void *
+dbm_mmap (dbm_memory_t *dbmp, size_t nbytes)
+{
+    caddr_t old_top;
+    size_t new_top;
+
+    old_top = (caddr_t)dbmp + dbmp->dm_top;
+    new_top = dbmp->dm_top + nbytes;
+
+    if (new_top > dbmp->dm_size)
+	return NULL;
+
+    dbmp->dm_top = new_top;
+
+    return old_top;
+}
+
 int
 main (int argc UNUSED, char **argv UNUSED)
 {
@@ -64,6 +86,48 @@ main (int argc UNUSED, char **argv UNUSED)
 
 	} else {
 	    print_help(cp);
+	}
+    }
+
+    dbm_memory_t *dbmp;
+    size_t init_size;
+    void *pointers[8<<10];
+    unsigned round, idx;
+    unsigned flags = DBMF_CREATE | DBMF_WRITE;
+
+    init_size = 32<<20;
+
+#if 0
+    dbmp->dm_size = init_size;
+    dbmp->dm_top = (sizeof(*dbmp) + DBM_PAGE_SIZE - 1) & ~DBM_PAGE_MASK;
+#else
+    dbmp = dbm_open("test.db", (caddr_t) DBM_COMPAT_ADDR,
+		    10, 10 * DBM_PAGE_SIZE, &flags);
+    if (dbmp == 0)
+	err(1, "bad dbmp");
+#endif
+
+    dbm_malloc_init(dbmp);
+
+    memset(pointers, 0, sizeof(pointers));
+
+    printf("memory: %p\n", dbmp);
+
+    for (round = 0; round < ROUNDS; round++) {
+	idx = random() % (sizeof(pointers) / sizeof(*pointers));
+
+	if (pointers[idx]) {
+	    dbm_free(dbmp, pointers[idx]);
+	    pointers[idx] = NULL;
+	} else {
+	    cp = pointers[idx] = dbm_malloc(dbmp, idx);
+	    if (cp)
+		memset(cp, 0x55, idx);
+	    dbm_offset_t off = dbm_offset_of(dbmp, cp);
+	    char *cp2 = dbm_pointer_of(dbmp, off);
+	    if (cp != cp2)
+		printf("%d: wrong: %p -> %lu ->  %p\n",
+		       round, cp, (unsigned long) off, cp2);
 	}
     }
 
