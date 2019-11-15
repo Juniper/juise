e4ccc4b73f8507743dac7c93c2335898b8f1873b
Phil Shafer <phil@juise.org>
2019-11-15 00:50:01 -0500

    remove non-clira

--- packaging/mac/clira-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CLIRA_ROOT=/Applications/Clira/.root
-
-export DYLD_LIBRARY_PATH="${CLIRA_ROOT}/usr/local/lib"
-export SLAX_EXTDIR="${CLIRA_ROOT}/usr/local/lib/slax/extensions"
-export CLIRA_HOME="${CLIRA_ROOT}/usr/local"
-
-cd ${CLIRA_ROOT} && sh usr/local/bin/run-clira stop
